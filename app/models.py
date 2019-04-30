ced3377a271fdaade1f624dfa8782a1edb70579a
HasithaAbeysekera <hasithaa@hotmail.com>
2019-04-30 13:46:08 +0800

    added db migration folder

--- app/models.py
+++ app/models.py
@@ -13,7 +13,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
     #check admin
     def __repr__(self):
         return '<User {}>'.format(self.username)    
