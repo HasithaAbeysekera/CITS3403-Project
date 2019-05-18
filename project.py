6699303b8a162a4d255b3d4e3c94dde399184428
HasithaAbeysekera <hasithaa@hotmail.com>
2019-05-18 22:27:31 +0800

    removed Post

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Post, Player, Polls, PollVote, PollPlayer
+from app.models import User, Player, Polls, PollVote, PollPlayer
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Player': Player, 'Polls': Polls, 'PollVote': PollVote, 'PollPlayer': PollPlayer}
+    return {'db': db, 'User': User, 'Player': Player, 'Polls': Polls, 'PollVote': PollVote, 'PollPlayer': PollPlayer}
